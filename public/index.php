5cb464c33623fee2f632f7a2049fce5146c8957d
Ole Wengler <amlor@Oles-iMac.fritz.box>
2021-04-26 17:12:23 +0200

    index.php for the entry point

---
+++ public/index.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+// Delegate static file requests back to the PHP built-in webserver
+if (PHP_SAPI === 'cli-server' && $_SERVER['SCRIPT_FILENAME'] !== __FILE__) {
+    return false;
+}
+
+chdir(dirname(__DIR__));
+require 'vendor/autoload.php';
+
+/**
+ * Self-called anonymous function that creates its own scope and keeps the global namespace clean.
+ */
+(function () {
+    /** @var \Psr\Container\ContainerInterface $container */
+    $container = require 'config/container.php';
+
+    /** @var \JobQueueHandler\Service\Runner $queueHandler */
+    $queueHandler = $container->get(\JobQueueHandler\Service\Runner::class);
+    echo $queueHandler->run();
+})();
