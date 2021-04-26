b2baa2ad5d664f0df2e4db45ebdd671bdba206e7
Ole Wengler <amlor@Oles-iMac.fritz.box>
2021-04-26 17:12:44 +0200

    config and DI service manager

---
+++ config/config.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types = 1);
+
+use Laminas\ConfigAggregator\ConfigAggregator;
+use Laminas\ConfigAggregator\PhpFileProvider;
+
+$aggregator = new ConfigAggregator(
+    [
+        \JobQueueHandler\ConfigProvider::class,
+        new PhpFileProvider(realpath(__DIR__) . '/autoload/{{,*.}global,{,*.}local}.php'),
+    ],
+);
+
+return $aggregator->getMergedConfig();
