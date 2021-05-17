eb6b9dbaa7d6ece21a74176f5253c9b0e116aaca
Ole Wengler <amlor@Oles-iMac.fritz.box>
2021-05-17 13:26:51 +0200

    add some config providers to the container

--- config/config.php
+++ config/config.php
@@ -7,7 +7,10 @@ use Laminas\ConfigAggregator\PhpFileProvider;
 
 $aggregator = new ConfigAggregator(
     [
+        \MiniCliRoutes\ConfigProvider::class,
         \JobQueueHandler\ConfigProvider::class,
+        \EventSystem\ConfigProvider::class,
+        \App\ConfigProvider::class,
         new PhpFileProvider(realpath(__DIR__) . '/autoload/{{,*.}global,{,*.}local}.php'),
     ],
 );
